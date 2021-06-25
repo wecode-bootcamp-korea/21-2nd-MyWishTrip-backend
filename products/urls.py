f58bd6a122413a1782df98d5c8efd12e0b6e20d0
pkc035 <pkc0305@gmail.com>
2021-06-25 14:11:20 +0900

    Add : 상세페이지 기능 구현 Modify : 재고량 확인 기능 추가 Modify : 리뷰 수정사항 수정 완료 Modify : prefetch 기능 수정 Modify : 리뷰 수정 사항 수정 완료

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views      import ProductView, OptionView
 
 urlpatterns = [
-    
+    path('/<int:product_id>', ProductView.as_view()),
+    path('/option', OptionView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+import json
+from django.db.models import query
+from django.db.models.query import Prefetch, QuerySet
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+from django.db.models import Sum
+
+from .models import Product,Date, Review, ReviewImage, Option
+
+class ProductView(View):
+    def get(self, request, product_id):
+        product = Product.objects.filter(id=product_id).prefetch_related(
+            'review_set', 'review_set__reviewimage_set', 'option_set', 'option_set__date_set').first()
+
+        if not product:
+            return JsonResponse({'message':'NOT EXIST'}, status=404)
+
+        total_score = product.review_set.aggregate(Sum('score'))['score__sum']/product.review_set.count()
+
+        result = {
+            'name'         : product.name,
+            'price'        : product.price,
+            'discount'     : product.discount,
+            'main_image'   : product.main_image,
+            'detail_image' : product.detail_image,
+            'total_score'  : total_score,
+            'total_review' : product.review_set.count(),
+            'reviews'      : [{
+                'contents'     : review.contents,
+                'score'        : review.score,
+                'manager_text' : review.manager_text,
+                'create_at'    : review.create_at,
+                'update_at'    : review.update_at,
+                'images'       : [data.image for data in review.reviewimage_set.all()]
+            }for review in product.review_set.all()],
+            'options'      : [{
+                'name'  : option.name,
+                'price' : option.price,
+                'dates' : [data.date for data in option.date_set.all()]
+            } for option in product.option_set.all()]
+        }
+
+        return JsonResponse({'product':result}, status=200)
+
+class OptionView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            date = Date.objects.get(id=data['option_id'])
+        
+            result = (True if date.count > data['count'] else False)
+
+            return JsonResponse({'status':result}, status=200)
+        except Date.DoesNotExist:
+            return JsonResponse({'message':'NOT EXIST'}, status=404)
+
+
+
