c65aaa52ea6761d6c3b75a7506758f942c7cb791
pkc035 <pkc0305@gmail.com>
2021-06-25 14:19:19 +0900

    Modify : social 필드 추가

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     name        = models.CharField(max_length=20)
     password    = models.CharField(max_length=200, null=True)
     signup_type = models.CharField(max_length=45)
+    social_id   = models.CharField(max_length=45, null=True)
     create_at   = models.DateTimeField(auto_now_add=True)
     update_at   = models.DateField(auto_now=True)
     options     = models.ManyToManyField('products.Option', through='reservations.Reservation')
