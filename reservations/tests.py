d51627ebb7eaf9475b411a582efef557680b3a51
pkc035 <pkc0305@gmail.com>
2021-06-25 13:43:37 +0900

    Add : Reservation 기능 추가 Modify : reservation 추가 수정 Modify : Test.py 작성 완료 Modify : Reservation 리뷰 수정 완료

--- reservations/tests.py
+++ reservations/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+import json
+import bcrypt
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from products.models import MainCategory, SubCategory, Region, Product, Option, Date
+from users.models    import User
+from unittest.mock   import MagicMock
+
+from decimal  import Decimal
+from datetime import datetime
+
+class ReservationTastCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        MainCategory.objects.create(id=1,name='test')
+        SubCategory.objects.create(id=1,name='test', maincategory_id=1)
+        Region.objects.create(id=1,name='test')
+        User.objects.create(
+            id=1,email='test@test.com',
+            name='test',
+            password=bcrypt.hashpw('test123456789'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            )
+        Product.objects.create(
+            id=1,
+            name='test',
+            price=Decimal(123.00),
+            discount=0.1,
+            main_image='test',
+            detail_image='test',
+            subcategory_id=1,
+            region_id=1
+        )
+        Option.objects.bulk_create(
+            [
+                Option(id=1, name='test', price=Decimal(1), product_id=1),
+                Option(id=2, name='test2', price=Decimal(1), product_id=1)
+            ]
+        )
+        Date.objects.bulk_create(
+            [
+                Date(id=1, date=datetime.strptime('2021-06-26', '%Y-%m-%d'), count=5, option_id=1),
+                Date(id=2, date=datetime.strptime('2021-06-27', '%Y-%m-%d'), count=5, option_id=1)
+            ]
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+        MainCategory.objects.all().delete()
+        SubCategory.objects.all().delete()
+        Region.objects.all().delete()
+        Product.objects.all().delete()
+        Date.objects.all().delete()
+        Option.objects.all().delete()
+    
+    def test_reservation_post(self):
+        client = Client()
+
+        body_data = {'reservations' : [{
+            'option_id' : 1,
+            'date_id'   : 1,
+            'count'     : 1
+        },
+        {
+            'option_id' : 1,
+            'date_id'   : 2,
+            'count'     : 1
+        }
+        ]}
+
+        login_data = {
+            'email' : 'test@test.com',
+            'password' : 'test123456789'
+        }
+        
+        login_response = client.post('/users/signin', json.dumps(login_data), content_type='application/json')
+    
+        headers = {
+            'HTTP_Authorization' : login_response.json()['token'],
+            'content_type' : 'application/json'
+        }
+
+        response = client.post('/reservations', json.dumps(body_data), **headers)
+        
+        self.assertEqual(response.json(), {'reservation':'SUCCESS'})
+        self.assertEqual(response.status_code, 201)
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+from django.db    import transaction
+
+from .models         import Reservation
+from users.utils     import user_decorator
+from products.models import Date
+
+class ReservationVeiw(View):
+    @user_decorator
+    @transaction.atomic
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        for data in data['reservations']:
+            reservation, created = Reservation.objects.get_or_create(
+                user_id = request.user.id,
+                option_id = data['option_id'],
+                count = 1)
+        
+            if created:
+                reservation.count = data["count"]
+            else:
+                reservation.count += data["count"]
+
+            reservation.save()
+
+            date = Date.objects.get(id=data['date_id'])
+            date.count -= data["count"]
+            date.save()
+
+        return JsonResponse({'reservations':'SUCCESS'}, status=201)
