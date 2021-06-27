ce0188970ba778dfa31f5c9a45d4dcda826721a6
Kevin <nellholic@gmail.com>
2021-06-27 19:47:17 +0900

    ADD : prdoucts list view
    ADD : regionsview and pagenation
    
    ADD : sql querylogging, search featrue
    
    MODIFY : code refatoring -products.views  & ADD products.test.py
    
    MODIFY : working on review_count duplicate issue
    
    MODIFY : fix rebase conflict
    
    MODIFY: apply code convention
    
    MODIFY: change query parameter get--> getlist , delete verbose exception
    
    MODIFY : tests.py
    
    FIX : products.url collision

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views      import ProductView, OptionView, ProductsView, RegionsView, ReviewView
 
 urlpatterns = [
-    path('',ReviewView.as_view()),
-    path('/<int:product_id>', ProductView.as_view()),
-    path('/option', OptionView.as_view()),
     path('', ProductsView.as_view()),
-    path('/regions',RegionsView.as_view())
+    path('/review',ReviewView.as_view()),
+    path('/option', OptionView.as_view()),
+    path('/regions',RegionsView.as_view()),
+    path('/<int:product_id>', ProductView.as_view())
 ]
