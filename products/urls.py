28ef38d706a4a990cd560dfa73eccd03ec210265
pkc035 <pkc0305@gmail.com>
2021-06-30 08:02:55 +0900

    Modify : result user name 추가 Modify : pagination 기능 구현 Modify : pagination 기능 수정 Modify : score 계산 수정 Modify : 리뷰 수정 사항 완료

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views      import ProductView, OptionView
+from .views      import ProductView, OptionView, ReviewView
 
 urlpatterns = [
+    path('',ReviewView.as_view()),
     path('/<int:product_id>', ProductView.as_view()),
     path('/option', OptionView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,22 +1,21 @@
 import json
-from django.db.models import query
-from django.db.models.query import Prefetch, QuerySet
 
-from django.http  import JsonResponse
-from django.views import View
-from django.db.models import Sum
+from django.http           import JsonResponse
+from django.views          import View
+from django.db.models      import Avg
 
-from .models import Product,Date, Review, ReviewImage, Option
+from .models import Product, Date, Review
 
 class ProductView(View):
     def get(self, request, product_id):
         product = Product.objects.filter(id=product_id).prefetch_related(
-            'review_set', 'review_set__reviewimage_set', 'option_set', 'option_set__date_set').first()
+            'review_set', 'review_set__user','review_set__reviewimage_set',
+            'option_set', 'option_set__date_set').first()
 
         if not product:
             return JsonResponse({'message':'NOT EXIST'}, status=404)
 
-        total_score = product.review_set.aggregate(Sum('score'))['score__sum']/product.review_set.count()
+        total_score = product.review_set.aggregate(Avg('score'))['score__avg']
 
         result = {
             'name'         : product.name,
@@ -24,9 +23,10 @@ class ProductView(View):
             'discount'     : product.discount,
             'main_image'   : product.main_image,
             'detail_image' : product.detail_image,
-            'total_score'  : total_score,
-            'total_review' : product.review_set.count(),
+            'total_score'  : total_score if total_score != None else 0,
+            'total_review' : product.review_set.count() if product.review_set.count() > 0 else 0,
             'reviews'      : [{
+                'user'         : review.user.name,
                 'contents'     : review.contents,
                 'score'        : review.score,
                 'manager_text' : review.manager_text,
@@ -52,8 +52,26 @@ class OptionView(View):
             result = (True if date.count > data['count'] else False)
 
             return JsonResponse({'status':result}, status=200)
+
         except Date.DoesNotExist:
             return JsonResponse({'message':'NOT EXIST'}, status=404)
 
+class ReviewView(View):
+    def get(self, request):
+        product_id = request.GET.get('product_id')
+        offset     = int(request.GET.get('offset',0))
+        limit      = int(request.GET.get('limit',3))
 
+        reviews = Review.objects.filter(product_id=product_id).prefetch_related('user','reviewimage_set')[offset:limit]
 
+        result = [{
+                'user'         : review.user.name,
+                'contents'     : review.contents,
+                'score'        : review.score,
+                'manager_text' : review.manager_text,
+                'create_at'    : review.create_at,
+                'update_at'    : review.update_at,
+                'images'       : [data.image for data in review.reviewimage_set.all()]
+                } for review in reviews]
+
+        return JsonResponse({'reviews':result}, status=200)
